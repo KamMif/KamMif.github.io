b44f02d9d3f0628b5062c225cb408dfe7f4cacfd
kmiftahov <kmiftahov@ivi.ru>
2019-04-21 17:58:22 +0400

    new_version-bootstrap-site

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const bodyParser = require("body-parser");
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hey, Im Node Js');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+// in latest body-parser use like below.
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/", function (request, response) {
+    console.log(request);
+    response.redirect('/');
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.use('/assets', express.static(__dirname + '/assets'));
 
 app.listen(3000, () => {
     console.log('Server is up on 3000');
